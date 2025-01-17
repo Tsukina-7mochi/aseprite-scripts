43e620db70788ce399b3b003e7bc87791807726c
Tsukina-7mochi <Tsukina7mochi@gmail.com>
2025-01-17 18:08:10 +0900

    build: make build environment

---
+++ smooth-filter/build.lua
@@ -0,0 +1,12 @@
+local neblua = require("lib.neblua")
+
+local distDir = os.getenv("DIST_DIR")
+
+neblua.bundle {
+    entry = "main",
+    output = distDir .. "/Smooth Filter.lua",
+    include = {
+        "./main.lua",
+    },
+    rootDir = "./smooth-filter",
+}
