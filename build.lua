89f5969dc24510414e07f7ca8b242213c9042686
Mooncake Sugar <Tsukina7mochi@gmail.com>
2025-11-10 21:01:18 +0900

    feat: add pre-init and post-run code to cleanup modules

--- build.lua
+++ build.lua
@@ -1,5 +1,43 @@
 local neblua = require("lib.neblua")
 
+local preInitCode = [[
+    local function snapshot(target)
+        local addedKeys = {}
+        setmetatable(target, {
+            __index = function(t, k)
+                if k == "__unload" then
+                    return function()
+                        for _, k in ipairs(addedKeys) do
+                            rawset(t, k, nil)
+                        end
+                    end
+                end
+
+                return rawget(t, k)
+            end,
+            __newindex = function (t, k, v)
+                table.insert(addedKeys, k)
+                rawset(t, k, v)
+            end,
+        })
+        return target
+    end
+
+    rawset(package, "loaded", snapshot(package.loaded))
+    rawset(package, "preload", snapshot(package.preload))
+    rawset(package, "searchers", snapshot(package.searchers))
+]]
+
+local postRunCode = [[
+    package.loaded.__unload()
+    package.preload.__unload()
+    package.searchers.__unload()
+
+    setmetatable(package.loaded, nil)
+    setmetatable(package.preload, nil)
+    setmetatable(package.searchers, nil)
+]]
+
 neblua.bundle({
     entry = "entry.iconCursor",
     output = "./icon-and-cursor/Export as ico cur ani.lua",
@@ -8,6 +46,8 @@ neblua.bundle({
     },
     rootDir = "./src",
     fallbackStderr = true,
+    preInitCode = preInitCode,
+    postRunCode = postRunCode,
 })
 
 neblua.bundle({
@@ -18,6 +58,8 @@ neblua.bundle({
     },
     rootDir = "./src",
     fallbackStderr = true,
+    preInitCode = preInitCode,
+    postRunCode = postRunCode,
 })
 
 neblua.bundle({
@@ -28,6 +70,8 @@ neblua.bundle({
     },
     rootDir = "./src",
     fallbackStderr = true,
+    preInitCode = preInitCode,
+    postRunCode = postRunCode,
 })
 
 neblua.bundle({
@@ -38,4 +82,6 @@ neblua.bundle({
     },
     rootDir = "./src",
     fallbackStderr = true,
+    preInitCode = preInitCode,
+    postRunCode = postRunCode,
 })
