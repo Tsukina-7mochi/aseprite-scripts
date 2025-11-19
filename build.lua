aa5693215911de9c4fe7a319cab087dfc72e144d
Mooncake Sugar <Tsukina7mochi@gmail.com>
2025-11-19 13:49:36 +0900

    build: refactor bootstrap code

--- build.lua
+++ build.lua
@@ -4,38 +4,31 @@ local preInitCode = [[
     local function snapshot(target)
         local addedKeys = {}
         setmetatable(target, {
-            __index = function(t, k)
-                if k == "__unload" then
-                    return function()
-                        for _, k in ipairs(addedKeys) do
-                            rawset(t, k, nil)
-                        end
-                    end
-                end
-
-                return rawget(t, k)
-            end,
             __newindex = function (t, k, v)
                 table.insert(addedKeys, k)
                 rawset(t, k, v)
             end,
         })
-        return target
+
+        local rollback = function()
+            for _, k in ipairs(addedKeys) do
+                rawset(target, k, nil)
+            end
+            setmetatable(target, nil)
+        end
+
+        return rollback
     end
 
-    rawset(package, "loaded", snapshot(package.loaded))
-    rawset(package, "preload", snapshot(package.preload))
-    rawset(package, "searchers", snapshot(package.searchers))
+    local rollbackLoaded = snapshot(package.loaded)
+    local rollbackPreload = snapshot(package.preload)
+    local rollbackSearchers = snapshot(package.searchers)
 ]]
 
 local postRunCode = [[
-    package.loaded.__unload()
-    package.preload.__unload()
-    package.searchers.__unload()
-
-    setmetatable(package.loaded, nil)
-    setmetatable(package.preload, nil)
-    setmetatable(package.searchers, nil)
+    rollbackLoaded()
+    rollbackPreload()
+    rollbackSearchers()
 ]]
 
 neblua.bundle({
