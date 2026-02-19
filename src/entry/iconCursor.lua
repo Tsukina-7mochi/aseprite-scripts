8823470fea8d99e5c2a3d44d329e5fa900ca42cf
Mooncake Sugar <Tsukina7mochi@gmail.com>
2026-02-20 00:00:50 +0900

    feat(icon-cursor): bump version to v0.2.0

--- src/entry/iconCursor.lua
+++ src/entry/iconCursor.lua
@@ -1,7 +1,7 @@
 package.manifest = {
     name = "aseprite-scripts/icon-and-cursor",
     description = "Export sprite as windows icon and cursor.",
-    version = "v0.1.1",
+    version = "v0.2.0",
     author = "Mooncake Sugar",
     license = "MIT",
     homepage = "https://github.com/Tsukina-7mochi/aseprite-scripts/blob/master/icon-and-cursor/",
